eb985a72c553789448f3686a6e91a3c789d3e983
Nate1423 <123126228+Nate1423@users.noreply.github.com>
2025-08-14 03:54:26 +0200

    Add files via upload

---
+++ Weather.py
@@ -0,0 +1,21 @@
+import urllib.request
+import json
+
+API_KEY = "f493235022f117443717201d55ff1d93"
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather"
+
+city = input("Enter a city name: ")
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+
+try:
+    with urllib.request.urlopen(request_url) as response:
+        data = json.loads(response.read())
+        if data.get("cod") == 200:
+            weather = data['weather'][0]['description']
+            temperature = round(data["main"]["temp"] - 273.15, 2)
+            print("Weather:", weather)
+            print("Temperature:", temperature, "celsius")
+        else:
+            print("An error occurred:", data.get("message"))
+except Exception as e:
+    print("Request failed:", e)
